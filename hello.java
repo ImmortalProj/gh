5e350b92b8fc87f6e7e17b775d7c6b41372f6a97
ImmortalProj <ImmortalProj@users.noreply.github.com>
2021-08-12 14:21:51 +0300

    jhfgg

---
+++ hello.java
@@ -0,0 +1,15 @@
+package com.helloworld.demo;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		for (int i = 0; i < 2; i++)
+		{
+			System.out.println("Hello");
+		}
+		
+		while(true)
+		System.out.println("Hello");
+	}
+}
